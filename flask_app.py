becedf6c0dc04f4005263713cf2f1d4f778af460
apophis51 <malcolmxvernon@hotmail.com>
2023-02-01 06:51:54 -0500

    v

--- flask_app.py
+++ flask_app.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
