4ec2d9e6cbb769df6944cf687f6285a55d5dfe59
apophis51 <malcolmxvernon@hotmail.com>
2023-02-26 12:54:52 -0500

    3423

--- flask_app.py
+++ flask_app.py
@@ -10,3 +10,14 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/leetcode')
+def leetcode():
+    return render_template("leet-code-index.html")
+
+@app.route('/leetcode/1')
+def num_1():
+    return render_template("/leetcode/1.html")
+
+@app.route('/test')
+def test():
+    return render_template("test.html")
